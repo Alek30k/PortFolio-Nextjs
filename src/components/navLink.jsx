f9aafb4fdb72008845e8532b4d2775c706c93b3a
Alek30k <alemank23k@gmail.com>
2024-03-17 16:42:40 -0300

    last commit

---
+++ src/components/navLink.jsx
@@ -0,0 +1,16 @@
+"use clinet";
+
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+const NavLink = ({ link }) => {
+  const pathName = usePathname();
+
+  return (
+    <Link className="" href={link.url}>
+      {link.title}
+    </Link>
+  );
+};
+
+export default NavLink;
