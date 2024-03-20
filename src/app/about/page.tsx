7b4fc1c6b488bbf6f304a57011274e9079cdcf05
Alek30k <alemank23k@gmail.com>
2024-03-20 11:48:55 -0300

    last commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,21 @@
 "use client";
 
-import { motion } from "framer-motion";
+import { motion, useInView, useScroll } from "framer-motion";
+import Brain from "../../components/Brain";
+import { useRef } from "react";
 
 const AboutPage = () => {
+  const containerRef = useRef();
+
+  const { scrollYProgress } = useScroll({ container: containerRef });
+
+  const skillRef = useRef();
+  // const isSkillRefInView = useInView(skillRef, {once:true});
+  const isSkillRefInView = useInView(skillRef, { margin: "-100px" });
+
+  const experienceRef = useRef();
+  const isExperienceRefInView = useInView(experienceRef, { margin: "-100px" });
+
   return (
     <motion.div
       className="h-full "
@@ -12,7 +25,7 @@ const AboutPage = () => {
     >
       {/* CONTAINER */}
 
-      <div className="h-full overflow-scroll lg:flex">
+      <div className="h-full overflow-scroll lg:flex" ref={containerRef}>
         {/* TEXT CONTAINER */}
         <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pd-0 xl:1/2">
           {/* BIOGRAPHY CONTAINER */}
@@ -226,7 +239,7 @@ const AboutPage = () => {
         </div>
         {/* SVG CONTAINER */}
         <div className="hidden lg:block w-1/3 sticky top-0 z-30 xl:w-1/2">
-          {/* <Brain scrollYProgress={scrollYProgress} /> */}
+          <Brain scrollYProgress={scrollYProgress} />
         </div>
       </div>
     </motion.div>
