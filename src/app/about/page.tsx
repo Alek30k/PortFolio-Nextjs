918ed5513986f50971bf673c10baaa2b206120c4
Alek30k <alemank23k@gmail.com>
2024-03-20 11:35:59 -0300

    last commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,9 +12,9 @@ const AboutPage = () => {
     >
       {/* CONTAINER */}
 
-      <div className="">
+      <div className="h-full overflow-scroll lg:flex">
         {/* TEXT CONTAINER */}
-        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64">
+        <div className="p-4 sm:p-8 md:p-12 lg:p-20 xl:p-48 flex flex-col gap-24 md:gap-32 lg:gap-48 xl:gap-64 lg:w-2/3 lg:pd-0 xl:1/2">
           {/* BIOGRAPHY CONTAINER */}
           <div className="flex flex-col gap-12 justify-center">
             {/* BIOGRAPHY TITLE */}
